a6f272aeee007725d77e3e7efc9bd42129d9dfd2
DiegoVilchezG <63325669+DiegoVilchezG@users.noreply.github.com>
2022-09-02 20:13:48 -0500

    Create namespaces.cpp

---
+++ namespaces.cpp
@@ -0,0 +1,9 @@
+#include "myNS.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  cout << "5! = " <<MyNS::factorial(5) << endl;
+  cout << "7+25 = " <<MyNS::Addition(7, 25) << endl;
+}
